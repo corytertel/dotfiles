aab9617f851d72c3547406d518397c166f52c028
corytertel <ctertel@comcast.net>
2021-07-24 13:56:48 -0700

    fixed battery

--- dwm-bar/bar-functions/executable_dwm_battery.sh
+++ dwm-bar/bar-functions/executable_dwm_battery.sh
@@ -6,15 +6,28 @@
 
 dwm_battery () {
     # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
-    CHARGE=$(cat /sys/class/power_supply/BAT1/capacity)
-    STATUS=$(cat /sys/class/power_supply/BAT1/status)
+    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
     printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "" ]; then
+    if [ "$IDENTIFIER" = "unicode" ]; then
         if [ "$STATUS" = "Charging" ]; then
-            printf " %s%% %s" "$CHARGE" "$STATUS"
+            #printf " %s%% %s" "$CHARGE" "$STATUS"
+            printf " %s%%" "$CHARGE"
         else
-            printf " %s%% %s" "$CHARGE" "$STATUS"
+            #printf " %s%% %s" "$CHARGE" "$STATUS"
+
+            if [ $CHARGE -gt 89 ]; then
+                printf "  %s%%" "$CHARGE"
+            elif [ $CHARGE -gt 69 ]; then
+                printf "  %s%%" "$CHARGE"
+            elif [ $CHARGE -gt 44 ]; then
+                printf "  %s%%" "$CHARGE"
+            elif [ $CHARGE -gt 19 ]; then
+                printf "  %s%%" "$CHARGE"
+            else
+                printf "  %s%%" "$CHARGE"
+            fi
         fi
     else
         printf "BAT %s%% %s" "$CHARGE" "$STATUS"
